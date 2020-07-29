56b4e42bbb52587a0c849de8826c9e424e1a43d1
adimajo <ztech1.25@gmail.com>
2020-07-29 18:42:48 +0200

    added tests and argument checking

--- tests/testthat/test-glmdisc.R
+++ tests/testthat/test-glmdisc.R
@@ -44,4 +44,34 @@ test_that("glmdisc warns for type of input data", {
      expect_warning(glmdisc(x, y, iter = 50, m_start = 4, interact = FALSE, test = FALSE, validation = TRUE, criterion = "bic"),
                     "No need to penalize the log-likelihood when a validation set is used. Using log-likelihood instead of AIC/BIC.",
                     fixed=TRUE)
+     
+     glmdisc(x, y, iter = 50, m_start = 4, interact = FALSE, test = FALSE, validation = FALSE, criterion = "bic")
+     
+     x[1, 1] <- NA
+     
+     glmdisc(x, y, iter = 50, m_start = 4, interact = FALSE, test = FALSE, validation = TRUE, criterion = "gini")
+})
+
+
+test_that("glmdisc m_start > nlevels categorical data", {
+     set.seed(1)
+     x <- matrix(runif(300), nrow = 100, ncol = 3)
+     cuts <- seq(0, 1, length.out = 4)
+     xd <- apply(x, 2, function(col) as.numeric(cut(col, cuts)))
+     theta <- t(matrix(c(0, 0, 0, 2, 2, 2, -2, -2, -2), ncol = 3, nrow = 3))
+     log_odd <- rowSums(t(sapply(seq_along(xd[, 1]), function(row_id) sapply(seq_along(xd[row_id, ]), function(element) theta[xd[row_id, element], element]))))
+     y <- rbinom(100, 1, 1 / (1 + exp(-log_odd)))
+     xdata = data.frame(x.1 = factor(xd[, 1]), x.2 = factor(xd[, 2]), x.3 = factor(xd[, 3]))
+     glmdisc(xdata, y, iter = 50, m_start = 6, interact = FALSE, test = FALSE, validation = FALSE, criterion = "aic")
+})
+
+test_that("glmdisc stops early", {
+     set.seed(1)
+     x <- matrix(runif(300), nrow = 100, ncol = 3)
+     cuts <- seq(0, 1, length.out = 4)
+     xd <- apply(x, 2, function(col) as.numeric(cut(col, cuts)))
+     theta <- t(matrix(c(0, 0, 0, 2, 2, 2, -2, -2, -2), ncol = 3, nrow = 3))
+     log_odd <- rowSums(t(sapply(seq_along(xd[, 1]), function(row_id) sapply(seq_along(xd[row_id, ]), function(element) theta[xd[row_id, element], element]))))
+     y <- rbinom(100, 1, 1 / (1 + exp(-log_odd)))
+     glmdisc(x, y, iter = 2000, m_start = 3, interact = FALSE, test = FALSE, validation = FALSE, criterion = "aic")
 })
