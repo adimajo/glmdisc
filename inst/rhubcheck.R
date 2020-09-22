171a700d1facf1ecf27f4eb3e37f2b0bbd479c62
adimajo <ztech1.25@gmail.com>
2020-09-22 22:19:02 +0200

    some updates related to NOTE on CRAN submission for a deprecated link in the vignette

--- inst/rhubcheck.R
+++ inst/rhubcheck.R
@@ -12,7 +12,7 @@ token <- args[[2L]]
 if (!is.element(platform, rhub::platforms()[[1L]])) {
      stop(paste(platform, "not in rhub::platforms()[[1L]]"))
 }
-print(substr(utils::maintainer(pkg = "glmdisc"), regexec("<", utils::maintainer(pkg = "glmdisc"))[[1]][1] + 1, nchar(utils::maintainer(pkg = "glmdisc")) - 1))
+
 rhub::validate_email(email = substr(utils::maintainer(pkg = "glmdisc"), regexec("<", utils::maintainer(pkg = "glmdisc"))[[1]][1] + 1, nchar(utils::maintainer(pkg = "glmdisc")) - 1),
                      token = token)
 cr <- rhub::check(platform = platform, show_status = TRUE)
